0751ced7dd20d2c77d184e789043c5e158ce1e0c
Breno-programador <brenosousamd@gmail.com>
2023-03-13 20:41:14 -0300

    novas divs

---
+++ ex21/scripts/script.js
@@ -0,0 +1,6 @@
+/* alert("Seja bem vindo ") /* Esse comando serve para exibirum alerta */
+/* confirm('Esses são apenas alguns comandos,ainda temos muito para aprender')  */
+
+var none = prompt('Qual é seu nome')
+alert(`Seja bem vindo ${none}`)
+/* console.log(`Seja bem vindo ${none}`) */ /* coloquei o sinal de pontuação crase dentro dos parentese */
